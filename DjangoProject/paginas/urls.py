e7574281c9d4e6e5958d2465006d6e5ac01b6da9
vynijales <vynijales@gmail.com>
2024-05-03 19:45:39 -0300

    feat: create base template 'modelo.html'

--- DjangoProject/paginas/urls.py
+++ DjangoProject/paginas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ModeloView, SobreView
 
 urlpatterns = [
     # path('endereco/', IndexView.as_view(), name='nome-da-url'),
     path('', IndexView.as_view(), name='inicio'),
+    path('modelo/', ModeloView.as_view(), name='modelo'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
