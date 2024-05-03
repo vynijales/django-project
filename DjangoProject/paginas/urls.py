686d892e755dd0bc575f5e0464ac3387ea642b14
vynijales <vynijales@gmail.com>
2024-05-03 18:09:58 -0300

    refactor: index.html, urls.py, and views.py in paginas app

--- DjangoProject/paginas/urls.py
+++ DjangoProject/paginas/urls.py
@@ -3,5 +3,5 @@ from .views import IndexView
 
 urlpatterns = [
     # path('endereco/', IndexView.as_view(), name='nome-da-url'),
-    path('inicio/', IndexView.as_view(), name='inicio'),
+    path('', IndexView.as_view(), name='inicio'),
 ]
--- DjangoProject/paginas/views.py
+++ DjangoProject/paginas/views.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'paginas/index.html'
